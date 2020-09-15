1f2d701e54720f873104b9c7bf2f1f5b9c5f94b5
bradserlis <bradserlis@yahoo.com>
2020-09-15 12:27:12 -0700

    cleanup image paths, remove excess from manifest

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import '../App.css';
+import logo from '../logo.jpg';
+import logoVerical from '../logo-vertical.jpg';
 
 const Headline = () => {
   return (
     <div className="headline">
       <h1 className="sr-only">Kailua House Cleaning</h1>
-      <img className="landscape-logo" src="/logo.jpg" alt="Kailua House Cleaning" />
-      <img className="default-logo" src="/logo-vertical.jpg" alt="Kailua House Cleaning" /> 
+      <img className="landscape-logo" src={logo} alt="Kailua House Cleaning" />
+      <img className="default-logo" src={logoVertical} alt="Kailua House Cleaning" /> 
     </div>
   )
 }
