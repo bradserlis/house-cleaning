bb0ebd367a320aad6f5a45232898765ce598ae5a
bradserlis <bradserlis@yahoo.com>
2020-09-16 12:36:35 -0700

    Updates

---
+++ precache-manifest.6473aca79eb6e0640029f9de92d8f4f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b239f815c88259ac66ff2df61c9c73a",
+    "url": "/bradserlis.github.io/kailua-house-cleaning/index.html"
+  },
+  {
+    "revision": "1f4b07cac68efa96a01e",
+    "url": "/bradserlis.github.io/kailua-house-cleaning/static/css/main.a2e882b6.chunk.css"
+  },
+  {
+    "revision": "03ae7b2998af0ca73592",
+    "url": "/bradserlis.github.io/kailua-house-cleaning/static/js/2.22a6b1aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bradserlis.github.io/kailua-house-cleaning/static/js/2.22a6b1aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f4b07cac68efa96a01e",
+    "url": "/bradserlis.github.io/kailua-house-cleaning/static/js/main.fdbbf89b.chunk.js"
+  },
+  {
+    "revision": "27136ac5fc76a113183c",
+    "url": "/bradserlis.github.io/kailua-house-cleaning/static/js/runtime-main.cb52b61a.js"
+  },
+  {
+    "revision": "a2b98cfe5358849250182fb2626bbc42",
+    "url": "/bradserlis.github.io/kailua-house-cleaning/static/media/logo-vertical.a2b98cfe.jpg"
+  },
+  {
+    "revision": "d1dd934ca94be2e2d983016ea51e4a99",
+    "url": "/bradserlis.github.io/kailua-house-cleaning/static/media/logo.d1dd934c.jpg"
+  }
+]);
